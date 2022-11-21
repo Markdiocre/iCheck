f212dd326e6c07e257c58f57de5692397337f971
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-11-21 22:06:35 +0800

    delete old button form

--- frontend/pages/home.js
+++ frontend/pages/home.js
@@ -4,6 +4,7 @@ import logout from "../public/logout.png";
 import styles from "../styles/Home.module.css";
 import React from "react";
 import Link from "next/link";
+import ButtonformComponent from "../components/button";
 
 const Viewport = ({ w }) => {
   if (w < 900) {
@@ -43,113 +44,8 @@ const Mobile = () => {
         <div className={styles.box2}>
           <p className={styles.disclaimersentence}>Do you Experience:</p>
           <div>
-            {/*FIRST ROW OF BUTTONS*/}
-            <div className={styles.buttonsdivrow}>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES} type="button">
-                  Cough and/or Colds
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES}>
-                  Sore Throat
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-            </div>
-            {/*SECOND ROW OF BUTTONS*/}
-            <div className={styles.buttonsdivrow}>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES}>
-                  Fatigue / Tireness
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES}>
-                  Headache
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-            </div>
-            {/*THIRD ROW OF BUTTONS*/}
-            <div className={styles.buttonsdivrow}>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES}>
-                  Body Pains
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES}>
-                  Dizzininess
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-            </div>
-            {/*FOURTH ROW OF BUTTONS*/}
-            <div className={styles.buttonsdivrow}>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES}>
-                  Loss of Taste or Smell
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES}>
-                  Difficulty Breathing
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-            </div>
-            {/*FIFTH ROW OF BUTTONS*/}
-            <div className={styles.buttonsdivrow}>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES}>
-                  Fever
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonYES}>
-                  Diarrhea
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-            </div>
-            {/*FIRST ROW OF BUTTONS QUESTION*/}
-            <div className={styles.buttonsdiv1}>
-              <button className={styles.buttonquestion1YES}>
-                <div className={styles.textalign}>
-                  Did you have any of the symptoms above in the last 3 days
-                </div>
-                <div className={styles.textalign}>
-                  <p className={styles.yesandno}>YES</p>
-                </div>
-              </button>
-            </div>
-            {/*SECOND ROW OF BUTTONS QUESTION*/}
-            <div className={styles.buttonsdivrow}>
-              <div className={styles.buttonsdiv}>
-                <button className={styles.buttonquestion2YES}>
-                  Have you had face-to-face contact with a probable or confirmed
-                  Covid 19 case within 1 meter and for more than 15 minutes for
-                  the past 14 days?
-                  <p className={styles.yesandno}>YES</p>
-                </button>
-              </div>
-              <div className={styles.buttonsdiv}>
-                <div className={styles.buttonsdiv}>
-                  <button className={styles.buttonquestion2YES}>
-                    Have you provided direct care for a patient with probable or
-                    confirmed COVID 19 case without using proper personal
-                    protective equipment for the past 14 days?
-                    <p className={styles.yesandno}>YES</p>
-                  </button>
-                </div>
-              </div>
-            </div>
+            <ButtonformComponent />
+
             {/*STILL NEEDS NEXT.JS VALIDATION*/}
             <div className={styles.tempdiv}>
               <form className={styles.tempformdiv} action="/send-data-here">
