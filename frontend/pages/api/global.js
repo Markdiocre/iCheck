39b992ca71b585fa61e08a264969c3cbc23a5451
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-11-22 19:33:23 +0800

    api login

---
+++ frontend/pages/api/global.js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+function handler(req, res) {
+  res.status(200).json({ name: "John Doe" });
+}
+const login = () => {
+  return;
+};
+export default { handler };
